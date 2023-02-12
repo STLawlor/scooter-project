ed804d763a4b194510a486d6af9a3a0a4cc557f9
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-02-12 16:28:59 +0000

    Add scooter class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,51 @@
-class Scooter{
-  // scooter code here
+class Scooter {
+  static nextSerial = 1;
+
+  constructor(station) {
+    this.station = station;
+    this.user = null; // null if docked
+    this.serialNum = Scooter.nextSerial;
+    Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+
+  rent() {
+    if (this.charge > 20 && !this.isBroken) {
+      return true;
+    } else {
+      if (this.charge <= 20) {
+        throw new Error("scooter needs to be charged");
+      }
+      if (this.isBroken) {
+        throw new Error("scooter needs repair");
+      }
+    }
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+
+  async recharge() {
+    while (this.charge < 100) {
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      if (this.charge <= 90) {
+        this.charge = this.charge + 10;
+      } else {
+        this.charge = 100;
+      }
+
+      console.log(`New charge ${this.charge}`);
+    }
+  }
+
+  async requestRepair() {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    this.isBroken = false;
+    console.log("repair completed");
+  }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,101 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+jest.setTimeout(7000);
 
-//typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+describe("Scooter", () => {
+  beforeAll(() => {
+    scooter1 = new Scooter("Piccadily");
+    scooter2 = new Scooter("Oxford");
+    logSpy = jest.spyOn(console, "log");
+  });
 
-//Method tests
-describe('scooter methods', () => {
-  // tests here!
+  describe("Scooter constructor", () => {
+    it("creates new Scooter instance", () => {
+      expect(scooter1).toBeInstanceOf(Scooter);
+    });
 
-  //rent method
+    it("initialises the station property", () => {
+      expect(scooter1.station).toBe("Piccadily");
+    });
 
-  //dock method
+    it("initialises the user property", () => {
+      expect(scooter1.user).toBe(null);
+    });
 
-  //requestRepair method
+    it("initialises the charge property", () => {
+      expect(scooter1.charge).toBe(100);
+    });
 
-  //charge method
+    it("initialises the isBroken property", () => {
+      expect(scooter1.isBroken).toBe(false);
+    });
 
-})
+    it("initialises the serialNum property", () => {
+      let serialNumType = typeof scooter1.serialNum;
+      expect(serialNumType).toBe("number");
+    });
+
+    it("increments the nextSerial static property", () => {
+      expect(scooter2.serialNum).toBe(scooter1.serialNum + 1);
+    });
+  });
+
+  describe("rent()", () => {
+    it("returns true if scooter is charged and not broken", () => {
+      let scooterAvialable = scooter1.rent();
+      expect(scooterAvialable).toBe(true);
+    });
+
+    it("throws error if scooters charge is below 21", () => {
+      scooter1.charge = 20;
+      expect(() => {
+        scooter1.rent();
+      }).toThrowError("scooter needs to be charged");
+    });
+
+    it("throws error if scooter is broken", () => {
+      scooter1.charge = 100;
+      scooter1.isBroken = true;
+      expect(() => {
+        scooter1.rent();
+      }).toThrowError("scooter needs repair");
+    });
+  });
+
+  describe("dock(station)", () => {
+    it("updates the scooters station", () => {
+      scooter1.dock("Victoria");
+      expect(scooter1.station).toBe("Victoria");
+    });
+
+    it("updates the scooters user", () => {
+      expect(scooter1.user).toBe(null);
+    });
+  });
+
+  describe("async recharge()", () => {
+    it("updates the scooters charge", async () => {
+      scooter1.charge = 90;
+      await scooter1.recharge();
+      expect(scooter1.charge).toBe(100);
+    });
+
+    it("console.logs the new charge", async () => {
+      scooter1.charge = 90;
+      await scooter1.recharge();
+      expect(logSpy).toHaveBeenCalledWith("New charge 100");
+    });
+  });
+
+  describe("async requestRepair()", () => {
+    it("updates the scooter isBroken property", async () => {
+      await scooter1.requestRepair();
+      expect(scooter1.isBroken).toBe(false);
+    });
+
+    it("console.logs when completed", async () => {
+      scooter1.isBroken = true;
+      await scooter1.requestRepair();
+      expect(logSpy).toHaveBeenCalledWith("repair completed");
+    });
+  });
+});
