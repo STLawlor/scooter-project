66676f1a52ef708617fe6a8d1604120bbf761f63
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-02-12 16:28:39 +0000

    Add user class

--- src/User.js
+++ src/User.js
@@ -1,5 +1,22 @@
 class User {
-  // User code here
+  constructor(userObj) {
+    this.username = userObj.username;
+    this.password = userObj.password;
+    this.age = userObj.age;
+    this.loggedIn = false;
+  }
+
+  login(password) {
+    if (password === this.password) {
+      this.loggedIn = true;
+    } else {
+      throw new Error("incorrect password");
+    }
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
 }
 
-module.exports = User
+module.exports = User;
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,57 @@
-const User = require('../src/User')
+const User = require("../src/User");
 
-// User tests here
+describe("User", () => {
+  beforeAll(() => {
+    user1 = new User({
+      username: "ScootyMcScooterson",
+      password: "ScootinDownTheStreetInMy64",
+      age: 99,
+    });
+  });
 
-// test username
+  describe("User constructor", () => {
+    it("creates new User instance", () => {
+      expect(user1).toBeInstanceOf(User);
+    });
 
-// test password
+    it("initialises the username property", () => {
+      expect(user1).toHaveProperty("username");
+      expect(user1.username).toBe("ScootyMcScooterson");
+    });
 
-// test age
+    it("initialises the password property", () => {
+      expect(user1).toHaveProperty("password");
+      expect(user1.password).toBe("ScootinDownTheStreetInMy64");
+    });
 
-// test login
+    it("initialises the age property", () => {
+      expect(user1).toHaveProperty("age");
+      expect(user1.age).toBe(99);
+    });
 
-// test logout
+    it("initialises the loggedIn property", () => {
+      expect(user1).toHaveProperty("loggedIn");
+      expect(user1.loggedIn).toBe(false);
+    });
+  });
+
+  describe("login()", () => {
+    it("logs user in if password is correct", () => {
+      user1.login(user1.password);
+      expect(user1.loggedIn).toBe(true);
+    });
+
+    it("throws error if password is incorrect", () => {
+      expect(() => {
+        user1.login("incorrectPassword");
+      }).toThrowError("incorrect password");
+    });
+  });
+
+  describe("logout()", () => {
+    it("logs user out", () => {
+      user1.logout();
+      expect(user1.loggedIn).toBe(false);
+    });
+  });
+});
